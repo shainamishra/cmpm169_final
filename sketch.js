a4d2ae6064c1a6c84954dd7334763ebc0e32b39d
DarenBartolucci <81998599+DarenBartolucci@users.noreply.github.com>
2022-03-07 14:10:14 -0800

    Changed sine wave function to create a sky

--- sketch.js
+++ sketch.js
@@ -1,9 +1,18 @@
 let dx;
 let yvalues;
 let theta = 0.0;
+var offset = 0;
+var angle = 0.0;
+var scalar = 40;
+var speed = 0.05;
+var sign = 1;
+
+function preload(){
+  img = loadImage('boat.png');
+}
 
 function setup() {
-  createCanvas(400, 400);
+  createCanvas(800, 800);
   dx = (TWO_PI / 1000) * 4;
   yvalues = new Array(floor(width/2))
 }
@@ -12,6 +21,7 @@ function draw() {
   background(0,48,47);
   calcWave();
   renderWave();
+  drawBoat();
 }
 
 function calcWave(){
@@ -25,8 +35,31 @@ function calcWave(){
 
 function renderWave(){
   noStroke();
-  fill(255);
+  fill(0);
+  beginShape(TESS);
+  vertex(0,0);
   for (let x = 0; x <yvalues.length; x++){
-    ellipse(x * 2, height * 1/4 + yvalues[x], 1, 1);
+    vertex(x * 2, height * 1/4 + yvalues[x]);
   }
+  vertex(width,0);
+  endShape();
+}
+
+function drawBoat() {
+  // boat oscillation 
+  var x1 = (((sin(angle) * scalar/0.4) + 40) * sign) + offset;
+  var y1 = (sin(angle) * scalar/0.75) + 60;
+
+  // change sign so it flips direction
+  if(y1>113.315){
+    sign *=(-1);
+    if(sign == 1){ 
+      offset = 0;
+    }else{
+      offset = 2*x1;
+    }
+  }
+  // move picture
+  image(img,x1,y1, 400,240);
+  angle += speed;
 }
